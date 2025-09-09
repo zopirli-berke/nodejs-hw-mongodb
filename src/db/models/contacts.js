b285b6cb204dd0438cbda88a7bac2692bcf3370e
Berke <zberke97@gmail.com>
2025-09-09 16:54:47 +0300

    feat: Add contact schema model for MongoDB

---
+++ src/db/models/contacts.js
@@ -0,0 +1,28 @@
+import { model, Schema } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
